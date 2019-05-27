6745ab32e34e823d8fd0dce60792cffa5a43cacd
Stephen Russell Crane <stephenrussellcrane@gmail.com>
2019-05-27 12:55:38 -0500

    installing sinon. adding global hooks for mocha

--- test/hooks.spec.js
+++ test/hooks.spec.js
@@ -1,5 +1,9 @@
-const Chance = require('Chance');
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+import Chance from 'chance';
 
 const chance = new Chance();
 
+chai.should();
+chai.use(sinonChai);
 global.chance = chance;
