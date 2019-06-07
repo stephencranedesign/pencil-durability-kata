780707b9be30daf09f72ecc51a150120a75788f7
Stephen Russell Crane <stephenrussellcrane@gmail.com>
2019-06-07 18:04:46 -0500

    adding proxyquire. updating mocha hooks

--- test/hooks.spec.js
+++ test/hooks.spec.js
@@ -7,3 +7,9 @@ const chance = new Chance();
 chai.should();
 chai.use(sinonChai);
 global.chance = chance;
+
+chance.array = () => chance.n(chance.string, chance.integer({min: 1, max: 20}));
+chance.object = () => Array(chance.d20() + 1).fill().reduce((acc, curr) => ({
+    ...acc,
+    [chance.string()]: chance.string()
+}), {});
