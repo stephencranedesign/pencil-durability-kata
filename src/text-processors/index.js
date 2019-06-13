ad396bea2167edc283dc755195c9d724a1582968
Stephen Russell Crane <stephenrussellcrane@gmail.com>
2019-06-13 07:18:33 -0500

    exposing editAndTrackCost

--- src/text-processors/index.js
+++ src/text-processors/index.js
@@ -1,7 +1,9 @@
 import {writeAndTrackCost} from './write-and-track-cost';
 import {eraseAndTrackCost} from './erase-and-track-cost';
+import {editAndTrackCost} from './edit-and-track-cost';
 
 export {
     writeAndTrackCost,
-    eraseAndTrackCost
+    eraseAndTrackCost,
+    editAndTrackCost
 };
--- test/unit/text-processors/index.spec.js
+++ test/unit/text-processors/index.spec.js
@@ -1,10 +1,12 @@
 import {writeAndTrackCost} from '../../../src/text-processors/write-and-track-cost';
 import {eraseAndTrackCost} from '../../../src/text-processors/erase-and-track-cost';
+import {editAndTrackCost} from '../../../src/text-processors/edit-and-track-cost';
 import * as textProcessors from '../../../src/text-processors';
 
 describe('Text Processors: index', () => {
     it('should export correct functions', () => {
         expect(textProcessors.writeAndTrackCost).to.equal(writeAndTrackCost);
         expect(textProcessors.eraseAndTrackCost).to.equal(eraseAndTrackCost);
+        expect(textProcessors.editAndTrackCost).to.equal(editAndTrackCost);
     });
 });
